e07d24b24a32feb0ca8a6f6757ca7f7c9dfe8b74
oolimry <limruiy@gmail.com>
2026-08-14 12:10:22 +0800

    add basic slash (sideways) and implementation for slashable obejcts

---
+++ Actors/WorldObjects/PlaceholderSlashableObject.gd
@@ -0,0 +1,5 @@
+class_name PlaceholderSlashableObject
+extends StaticBody2D
+
+func onSlash():
+	self.queue_free()
---
+++ Globals/ScriptConstants.gd
@@ -0,0 +1,6 @@
+class_name ScriptConstants
+extends RefCounted
+
+## all slashable objects should have this method name
+## any object that has layer Slashable and have this method defined is interactable by slash
+const ON_SLASH_METHOD_NAME = "onSlash"
