017e2684cdf92b6d253acb8fe1720423c6ac84c3
James <73779218+Somebody-somewher@users.noreply.github.com>
2026-08-15 15:49:33 +0800

    Fix boundary glitchiness

--- Actors/World/Room Management/RoomBoundary.gd
+++ Actors/World/Room Management/RoomBoundary.gd
@@ -5,13 +5,8 @@ signal requestChangeRoom(room)
 ## Location of the Boundary within the Room
 @export var location : ORIENTATION
 
-var direction = { 
-	ORIENTATION.WEST: Vector2.LEFT,
-	ORIENTATION.EAST: Vector2.RIGHT,
-	ORIENTATION.NORTH: Vector2.UP,
-	ORIENTATION.SOUTH: Vector2.DOWN,
-}[location] 
-
+var playerBody : Node2D
+var oriPlayerPos : Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,19 +19,29 @@ func _process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body is not Player:
 		return 
-		
+	
+	playerBody = body
+	oriPlayerPos = body.global_position
+
+func _on_body_exited(body: Node2D) -> void:
+	if !is_instance_valid(playerBody) or !is_same(body, playerBody) or !oriPlayerPos.is_finite():
+		return
+	
 	var offset := body.global_position - global_position
 	var is_valid_entry := false
 	
 	match location:
-		ORIENTATION.WEST:
-			is_valid_entry = body.global_position.x > global_position.x
 		ORIENTATION.EAST:
-			is_valid_entry = body.global_position.x < global_position.x
-		ORIENTATION.NORTH:
-			is_valid_entry = body.global_position.y > global_position.y
+			is_valid_entry = oriPlayerPos.x < global_position.x and body.global_position.x > oriPlayerPos.x
+		ORIENTATION.WEST:
+			is_valid_entry = oriPlayerPos.x > global_position.x and body.global_position.x < oriPlayerPos.x
 		ORIENTATION.SOUTH:
-			is_valid_entry = body.global_position.y < global_position.y
+			is_valid_entry = oriPlayerPos.y < global_position.y and body.global_position.y > oriPlayerPos.y
+		ORIENTATION.NORTH:
+			is_valid_entry = oriPlayerPos.y > global_position.y and body.global_position.y < oriPlayerPos.y
 
 	if is_valid_entry:
 		requestChangeRoom.emit(location)
+
+	playerBody = null
+	oriPlayerPos = Vector2.INF
