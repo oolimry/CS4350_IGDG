a67a04f3b6b1eab892499d466595f3a4b5645c1f
James <73779218+Somebody-somewher@users.noreply.github.com>
2026-09-03 09:28:01 +0800

    Add basic snapshotting

---
+++ Actors/World/Room Management/ResidentSnapshot.gd
@@ -0,0 +1,10 @@
+class_name ResidentSnapshot
+extends RefCounted
+
+## Position local to the RoomInstance 
+var localPos: Vector2
+
+## Applicable only to Rigidbody2Ds
+var linearVelocity: Vector2
+var angularVelocity: float
+var sleeping: bool
--- Actors/World/Room Management/RoomInstance.gd
+++ Actors/World/Room Management/RoomInstance.gd
@@ -8,7 +8,7 @@ var roomPos : Vector2i
 
 @export var roomEntry : RoomEntry
 
-#@export var roomResidents : Array[]
+var roomResidents : Dictionary[Node2D, ResidentSnapshot]
  
 func setup(pos : Vector2i) -> void:
 	roomPos = pos
@@ -20,7 +20,34 @@ func setup(pos : Vector2i) -> void:
 		elif n is RoomEntry:
 			roomEntry = n
 			roomEntry.roomPos = roomPos	
+		elif n is Node2D and n.is_in_group("Interactable"):
+			roomResidents[n as Node2D] = null
+	capture_snapshot()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func capture_snapshot() -> void:
+	var snapshot := ResidentSnapshot.new()
+	var snapshotArray : Array[ResidentSnapshot]
+	
+	for n in roomResidents.keys():
+		# We assume all interactables have a roomResident object
+		assert(n.roomResident != null)
+	
+		snapshot.localPos = to_local(n.global_position)
+
+		if n is RigidBody2D:
+			snapshot.linearVelocity = n.linear_velocity
+			snapshot.angularVelocity = n.angular_velocity
+			snapshot.sleeping = n.sleeping
+		
+		roomResidents[n] = snapshot
+		
+	return
+
+func restore_snapshot(snapshot: ResidentSnapshot) -> void:
+	for n in roomResidents.keys():
+		n.local_position = snapshot.localPos
+		
+		if n is RigidBody2D:
+			n.linear_velocity = snapshot.linearVelocity
+			n.angular_velocity = snapshot.angularVelocity
+			n.sleeping = snapshot.sleeping
