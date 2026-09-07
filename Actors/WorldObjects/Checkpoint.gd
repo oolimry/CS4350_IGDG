73faee3801bd29c8781e6d02d88613718b5a74f1
James <73779218+Somebody-somewher@users.noreply.github.com>
2026-09-07 14:59:37 +0800

    Change Checkpoint

--- Actors/WorldObjects/Checkpoint.gd
+++ Actors/WorldObjects/Checkpoint.gd
@@ -1,3 +1,4 @@
+@tool
 class_name CheckPoint
 extends Node2D
 
@@ -6,11 +7,35 @@ signal checkPointReached(pos : Vector2i)
 @export var referenceImage : Sprite2D
 
 @export var roomPos : Vector2i
+@export var shouldSpawnPlayer := false
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	referenceImage.queue_free()
-	pass # Replace with function body.
+@export var reference_texture: Texture2D:
+	set(value):
+		reference_texture = value
+		queue_redraw()
+@export var textureOffset : Vector2
+
+@export var reference_scale := Vector2.ONE:
+	set(value):
+		reference_scale = value
+		queue_redraw()
+
+func _draw() -> void:
+	if not Engine.is_editor_hint() or reference_texture == null:
+		return
+
+	var draw_size := reference_texture.get_size() * reference_scale
+	var draw_position := Vector2(
+		-draw_size.x * 0.5 + textureOffset.x,
+		-draw_size.y * 0.5 + textureOffset.y
+	)
+
+	draw_texture_rect(
+		reference_texture,
+		Rect2(draw_position, draw_size),
+		false,
+		Color(0.0, 0.4, 1.0, 1.0)
+	)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is not Player:
