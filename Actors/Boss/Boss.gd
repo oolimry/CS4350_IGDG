121c10862cf2f592e6d3ede78361b6bfd361d61f
James <73779218+Somebody-somewher@users.noreply.github.com>
2026-09-16 00:07:51 +0800

    Fix boss spawning

--- Actors/Boss/Boss.gd
+++ Actors/Boss/Boss.gd
@@ -1,13 +1,13 @@
 class_name Boss
-extends CharacterBody2D
+extends Node2D
 
 @export var health : Health
 
 # Static factory function acting as a custom constructor
-static func create(startingPos : Vector2, startingHP := 10) -> Boss:
+static func create(basePos : Vector2, startingHP := 10) -> Boss:
 	var scene = load("uid://dkht80tf3wlnf") as PackedScene
 	var instance = scene.instantiate() as Boss
-	instance.global_position = startingPos
+	instance.global_position = basePos
 	
 	instance.health.maxHealth = startingHP
 	instance.health.currHealth = startingHP 
